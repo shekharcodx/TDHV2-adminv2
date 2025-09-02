8ac70a8db20097d871735874f8f6f7a69f410c4e
Muhammad Hamza <m.hamza7265@gmail.com>
2025-09-02 22:39:11 +0500

    fix test

--- src/pages/dash/Cards.jsx
+++ src/pages/dash/Cards.jsx
@@ -1,6 +1,6 @@
 // components/Cards.jsx
 import React from "react";
-import styles from "./Card.module.css";
+import styles from "./card.module.css";
 import Card from "./Card";
 import { FaUserPlus, FaUsers, FaBoxOpen, FaDollarSign } from "react-icons/fa";
 
